6f310214984f064e35daa1a093bd3102b2fc1ded
nikuz <micurino@gmail.com>
2023-08-08 14:43:03 -0700

    Initial Commit

---
+++ sadhana/sadhanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sadhanaApp.swift
+//  sadhana
+//
+//  Created by Nikolai Kuznetcov on 2023-08-08.
+//
+
+import SwiftUI
+
+@main
+struct sadhanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
